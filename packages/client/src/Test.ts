d56670a140f55ff03827b7499e27deb564fa5edf
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-10-25 12:22:26 -0400

    style: fix formatting with prettier

--- packages/client/src/Test.ts
+++ packages/client/src/Test.ts
@@ -1,7 +1,7 @@
 const message = "Hello World!";
 
-function myFunc(message: string): string{
-    return message;
+function myFunc(message: string): string {
+  return message;
 }
 
 console.log(myFunc(message));
